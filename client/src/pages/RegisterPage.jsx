56fff5a2faf6019fbf85aed83279bee486488784
chris_beltran <cristianb72@hotmail.com>
2024-01-21 01:56:30 -0600

    save user

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,12 +1,15 @@
 import {useForm} from 'react-hook-form';
+import {registerRequest} from '../api/auth.js';
+
 function RegisterPage(){
 
     const{register,handleSubmit}=useForm();
 
     return(
         <div className='bg-zinc-800 max-w-md p-10 rounded-md'>
-            <form onSubmit={handleSubmit(values=>{
-                console.log(values);
+            <form onSubmit= {handleSubmit(async values=>{
+               const response= await  registerRequest(values);
+               console.log(response);
             })}>
             <input type="text" {...register("username",{required:true})}
                 className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
--- src/app.js
+++ src/app.js
@@ -3,9 +3,10 @@ import morgan from 'morgan';
 import routes from './routes/auth_routes.js'
 import cookieParser from 'cookie-parser';
 import taskRoutes from './routes/tasks_routes.js';
-
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
