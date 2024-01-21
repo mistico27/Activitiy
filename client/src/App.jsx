27f086a6e5b73d834350d61ca5a8098df26ef3d6
chris_beltran <cristianb72@hotmail.com>
2024-01-21 01:23:16 -0600

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,18 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+
 function App(){
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Home Page</h1>} />
+        <Route path='/login' element={<h1>login Page</h1>} />
+        <Route path='/register' element={<h1>register Page</h1>} />
+        <Route path='/task' element={<h1>task Page</h1>} />
+        <Route path='/add-task' element={<h1>add -task Page</h1>} />
+        <Route path='/task/:id' element={<h1>get a task Page</h1>} />
+        <Route path='/profile' element={<h1>profile Page</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
