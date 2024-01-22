e04bfdeb111f3d277c6c5af1ef93f804e3f943f3
chris_beltran <cristianb72@hotmail.com>
2024-01-21 20:05:20 -0600

    save activity

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,14 +1,31 @@
 import{useForm} from 'react-hook-form';
 import { useTasks } from '../context/TaskContext';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate,useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function TaskformPage(){
-    const{register,handleSubmit} = useForm(); 
-    const {createTask} =useTasks();
+    const{register,handleSubmit,setValue} = useForm(); 
+    const {createTask,getTask, updateTask} =useTasks();
     const navigate = useNavigate();
+    const params = useParams();
+
+    useEffect(()=>{
+       async function loadTask() { 
+        if(params.id){
+            const task = await getTask(params.id);
+            setValue('title',task.title);
+            setValue('description',task.description);
+        }
+    }
+    loadTask();
+    },[])
 
     const onSubmit = handleSubmit((data)=>{
-        createTask(data);
+        if(params.id){
+            updateTask(params.id,data);
+        }else{
+            createTask(data);
+        }
         navigate('/tasks');
     });
 
@@ -23,7 +40,6 @@ function TaskformPage(){
             <textarea rows="3" placeholder="description"
             {...register("description")}
             className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-
             >
             </textarea>
             <button>
--- src/controllers/task_controller.js
+++ src/controllers/task_controller.js
@@ -3,14 +3,20 @@ import task from '../models/task_model.js'
 
 
 export const getTasks = async (req,res)=>{
+    try{
     const tasks = await task.find({
         user:req.user.id
     }).populate('user');
     res.json(tasks);
-
+}catch(error){
+    return res.status(400).json({
+        message:"task could not be created"
+    });
+}
 };
 
 export const createTasks = async (req,res)=>{
+    try{
     const{title,description,date}=req.body;
     const newTask =new task({
         title,
@@ -20,28 +26,49 @@ export const createTasks = async (req,res)=>{
     });
    const savedTask = await newTask.save();
    res.json(savedTask);
+}catch(error){
+    return res.status(400).json({
+        message:"task could not be created"
+    })
+}
 };
 
 export const getTask = async (req,res)=>{
+    try{
     const mytask =await task.findById(req.params.id).populate('user');
     if(!mytask){
         return res.status(404).send({message:error.message});
     }
     res.json(mytask);
-}
+    }catch(error){
+        return res.status(400).json({
+            message:"task not found"
+        })
+    }
+};
 
 
 export const updateTask = async (req,res)=>{
+    try{
     const mytask =await task.findByIdAndUpdate(req.params.id,req.body,{new:true});
     if(!mytask){
         return res.status(404).send({message:"task not found"});
     }
     res.json(mytask);
+    }catch(error){
+      return  res.status(404).json({message:"task could not be found"});
+    }
 }
+
+
 export const deleteTask = async (req,res)=>{
+    try{
     const mytask =await task.findByIdAndDelete(req.params.id);
     if(!mytask){
         return res.status(404).send({message:error.message});
     }
      res.status(204).json("task has been deleted successfully");
-}
+}catch(error){
+   return res.status(404).json({message:"task could not be deleted"});
+} 
+};
